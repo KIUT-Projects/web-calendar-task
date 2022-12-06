622997126dc07ce84267fe2647ae5e7007d92c89
UzSoftic <softuzb@gmail.com>
2022-12-06 19:37:02 +0500

    updated

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,12 @@
-let allReminders = [{
-    title: '',
-    date: '',
-    time: '',
-}];
+// let allReminders = [{
+//     title: '',
+//     date: '',
+//     time: '',
+// }];
 
-function add() {
+let allReminders = [];
+
+function create() {
     let text = document.getElementById('inputNomi').value;
     let date = document.getElementById('inputKun').value;
     let time = document.getElementById('inputSoat').value;
@@ -38,187 +40,50 @@ function add() {
         priority: priority
     });
 
-    show();
+    showReminders();
+
+    document.getElementById('inputNomi').value=""
+    document.getElementById('inputKun').value=""
+    document.getElementById('inputSoat').value=""
+    document.getElementById('inputDaraja').value=""
+    
 }
 
-function show() {
-    let table = '<table class = table><tr><th>№</th><th>Nomi</th><th>Kuni</th><th>Vaqti</th><th>Muhimlik</th><th></th></tr>';
-    let result = document.getElementById('js-qaydlar');
-
-    for (let i = 1; i < allReminders.length; i++) {
-        table += `
-        <tr>
-            <td>${i}</td>
-            <td>${allReminders[i].title}</td>
-            <td>${allReminders[i].date}</td>
-            <td>${allReminders[i].time}</td>
-            <td>${allReminders[i].priority}</td>
-            <td><button onclick = "editReminder()" id="edit-btn">Tahrirlash</button></td>
-            <td><button onclick = "deleteReminder()" id="delete-btn">Ochirish</button></td>
-        </tr>
-        `;
-    }
-
-    table += '</table>';
-    result.innerHTML = table;
-
-
-    // sorting table by clicking (start)
-    document.querySelector('table').onclick = function (e) {
-        if (e.target.textContent == 'Sana') {
-            allReminders.sort((a, b) => {
-                return new Date(a.date) - new Date(b.date);
-            });
-
-            show();
-        } else if (e.target.textContent == 'Vaqt') {
-            allReminders.sort((a, b) => {
-                return a.time.localeCompare(b.time);
-            });
-
-            show();
-        } else if (e.target.textContent == 'Matni') {
-            allReminders.sort((a, b) => {
-                return a.title.localeCompare(b.title);
-            });
-
-            show();
-        }
-    }
-    // sorting table by clicking (end)
+function edit(el){
+    
 }
 
-function editReminder() {
-    let tr = event.target.closest('tr');
-    if (!tr) return;
+function showReminders(){
 
-    let text = prompt('Yangi matnni kiriting:');
-    let date = prompt('Yangi sana:');
-    let time = prompt('Yangi vaqt:');
-    let priority = prompt('Yangi muhimlik darajasi:');
-
-    tr.innerHTML = `
-        <td>${tr.rowIndex}</td>
-        <td>${text}</td>
-        <td>${date}</td>
-        <td>${time}</td>
-        <td>${priority}</td>
-        <td><button onclick = "editReminder()" id="edit-btn">Tahrirlash</button></td>
-        <td><button onclick = "deleteReminder()" id="delete-btn">Ochirish</button></td>
-    `
-
-    allReminders[tr.rowIndex] = {
-        title: text,
-        date: date,
-        time: time,
-        priority: priority
-    }
-}
-
-function deleteReminder() {
-    let tr = event.target.closest('tr');
-    if (!tr) return;
-
-    if (confirm('Ochirishni istaysizmi ?')) {
-        allReminders.splice(tr.rowIndex, 1);
-        show();
-    }
-}
-
-
-/**
- * Calendar funksiyasi uchun hafta kunini qaytaradi 0=dushanba 6=yakshanba.
- * @param {date} date hozirgi sana.
- * @returns {number} hafta kunini qaytaradi son qiymatida (0-6, 0 dushanba).
- */
-
-function getDay(date) {
-    let day = date.getDay();
-    if (day == 0) day = 7;
-    return day - 1;
-}
-
-/**
- * Calendar qurish funksiyasi.
- * @param elem shu elementga kalendar ornatadi.
- */
-
-let curDate = new Date();
-let globalMonth = curDate.getMonth() + 1;
-let globalYear = curDate.getFullYear();
-
-function calendar(elem, year, month) {
-    elem = document.querySelector(elem);
-    let mon = month - 1;
-    let date = new Date(year, mon);
-
-    let table = `
-        <table>
+    var data = `<table class="table table-striped table-hover">
+        <thead>
             <tr>
-                <th>Du</th>
-                <th>Se</th>
-                <th>Ch</th>
-                <th>Pa</th>
-                <th>Ju</th>
-                <th>Sh</th>
-                <th>Ya</th>    
+                <th scope="col">Nomi</th>
+                <th scope="col">Kun</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
             </tr>
-            <tr>
-    `;
+    </thead>
+  <tbody>`;
 
-    for (let i = 0; i < getDay(date); i++) {
-        table += `<td></td>`;
+    for(reminder of allReminders){
+        console.log(reminder)
+        data += `<tr>
+        <th scope="row">` + reminder.title + `</th>
+        <td>` + reminder.date + `</td>
+        <td>` + reminder.time + `</td>
+        <td>` + reminder.priority + `</td>
+      </tr>`
     }
 
-    while (date.getMonth() == mon) {
-        table += `<td> ${date.getDate()}</td>`;
+    data += `</tbody></table>`;
 
-        if (getDay(date) % 7 == 6) {
-            table += `</tr><tr>`;
-        }
-        date.setDate(date.getDate() + 1);
-    }
+    document.querySelector('#js-qaydlar').innerHTML = data;
 
-    if (getDay(date) != 0) {
-        for (let i = getDay(date); i < 7; i++) {
-            table += '<td></td>';
-        }
-    }
-
-    table += '</tr></table>';
-    elem.innerHTML = table;
+    console.log(allReminders)
 }
 
-function showCalendar() {
-    let nextBtn = document.getElementById('next-btn');
-    let prevBtn = document.getElementById('prev-btn');
-
-    nextBtn.hidden = false;
-    prevBtn.hidden = false;
-
-    calendar('#js-kalendar', globalYear, globalMonth);
-
-    nextBtn.addEventListener('click', function () {
-        globalMonth++;
-        if (globalMonth > 12) {
-            globalMonth = 1;
-            globalYear++;
-        }
-        calendar('#js-kalendar', globalYear, globalMonth);
-    });
-
-    prevBtn.addEventListener('click', function () {
-        globalMonth--;
-        if (globalMonth < 1) {
-            globalMonth = 12;
-            globalYear--;
-        }
-        calendar('#js-kalendar', globalYear, globalMonth);
-    });
-}
 
 document.addEventListener("DOMContentLoaded", function(event){
     showCalendar();
   });
-
-
